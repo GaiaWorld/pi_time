e1e5ad3c5977952b020672ba3baf60467b0ac7b6
wzjsun <530739162@qq.com>
2022-09-27 10:27:13 +0800

    feat: 增加接口

--- src/wasm_instant.rs
+++ src/wasm_instant.rs
@@ -9,12 +9,56 @@ pub struct Instant(NotNan<f64>);
 
 
 impl Instant {
+	/// 返回与“现在”相对应的瞬间。
 	pub fn now () -> Instant {
 		Instant(unsafe { NotNan::new_unchecked(Date::now()) })
 	}
 
+	/// 返回自创建此瞬间以来经过的时间量。
+	/// # panic
+	/// 如果当前时间早于该时刻，则此函数可能会出现恐慌，如果Instant综合生成，则可能会发生这种情况。
 	pub fn elapsed(&self) -> Duration {
 		Duration::from_nanos(((Date::now() - *self.0) * 1000000.0) as u64)
+	}
+
+	/// 返回从另一时刻到这一时刻所经过的时间量，如果该时刻晚于这一时刻，则返回零持续时间。
+	/// # panic
+	/// 以前，如果earlier晚于，我们会惊慌失措self。目前，此方法已饱和以遵循标准库的行为。在某些情况下，未来的版本可能会重新引入恐慌。
+	pub fn duration_since(&self, earlier: Instant) -> Duration {
+		let r = self.0 - earlier.0;
+		if *r < 0.0 {
+			Duration::from_nanos(0)
+		} else {
+			Duration::from_nanos(((*r) * 1000000.0) as u64)
+		}
+	}
+
+	/// 返回从另一时刻到这一时刻所经过的时间量，如果该时刻晚于这一时刻，则返回无。
+	pub fn checked_duration_since(&self, earlier: Instant) -> Option<Duration> {
+		let r = self.0 - earlier.0;
+		if *r < 0.0 {
+			None
+		} else {
+			Some(Duration::from_nanos(((*r) * 1000000.0) as u64))
+		}
+	}
+
+	/// 返回从另一时刻到这一时刻所经过的时间量，如果该时刻晚于这一时刻，则返回零持续时间。
+	pub fn saturating_duration_since(&self, earlier: Instant) -> Duration {
+		let r = self.0 - earlier.0;
+		if *r < 0.0 {
+			Duration::from_nanos(0)
+		} else {
+			Duration::from_nanos(((*r) * 1000000.0) as u64)
+		}
+	}
+
+	pub fn checked_add(&self, duration: Duration) -> Option<Instant> {
+		Some(Self(self.0 + (rhs.as_secs_f64() * 1000.0)))
+	}
+
+	pub fn checked_sub(&self, duration: Duration) -> Option<Instant> {
+		Some(Self(self.0 - (rhs.as_secs_f64() * 1000.0)))
 	}
 }
 
